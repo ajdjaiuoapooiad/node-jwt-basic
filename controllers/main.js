9ed6cc1d4ad8f180539b04888fb8b21ad49e2be2
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-30 14:28:09 +0900

    159ch ok

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,24 @@
 const CustomAPIError = require("../errors/custom-error")
-
+const jwt = require('jsonwebtoken')
 
 const login = async (req,res) => {
     const  {username,password} = req.body
     
-    if(!username | !password){
+    if(!username || !password){
         throw new CustomAPIError('Please provide email and password',400)
     }
-    
-    res.send('Fake login route')
+
+
+    //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate()
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+
+  res.status(200).json({ msg: 'user created', token })
 }
 
 
