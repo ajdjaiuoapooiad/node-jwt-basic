401a981977f8aedc25ca3cedbb3a04381af2c6d8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-30 16:32:04 +0900

    o

--- controllers/main.js
+++ controllers/main.js
@@ -25,6 +25,8 @@ const login = async (req,res) => {
 
 const dashboard = async (req,res) => {
     const luckyNumber = Math.floor(Math.random()*100)
+    console.log(req.headers);
+    
     res.status(200).json({msg:`Hello john`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})  
 }
 
