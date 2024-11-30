5ceee5b8420b94b274668c4f2abd29716f50d9be
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-30 16:36:56 +0900

    162ch ok

--- controllers/main.js
+++ controllers/main.js
@@ -24,10 +24,16 @@ const login = async (req,res) => {
 
 
 const dashboard = async (req,res) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    console.log(req.headers);
-    
-    res.status(200).json({msg:`Hello john`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})  
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Bearer')){
+    throw new CustomAPIError('No token provided',401)
+  }
+  const token = authHeader.split(' ')[1]
+  console.log(token);
+  
+  const luckyNumber = Math.floor(Math.random()*100)
+  res.status(200).json({msg:`Hello john`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})  
 }
 
 
